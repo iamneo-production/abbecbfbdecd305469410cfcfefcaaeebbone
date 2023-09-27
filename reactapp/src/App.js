68c2227af07220b0063be22586efcc66be4ad225
Iamneo <techadmin@iamneo.ai>
2023-09-27 06:41:18 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -4,23 +4,23 @@ import React , {useState} from 'react';
 
 function App() {
   const[name,setName]=useState('');
-  const[greeting,]
+  const[greeting,setGreeting]=useState('');
+
+  const handleChange=(event)=>{
+    setName(event.target.value);
+  };
+
+  const handleSubmit=(event)=>{
+    event.preventDefault();
+
+    if(name.trim()!==''){
+      setGreeting('Hello,${name}!');
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h2>Hey!!! Greeting</h2>
+    <form onSubmit={handleSubmit}
     </div>
   );
 }
